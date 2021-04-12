7d142966fa52b5657a2c1a63aa9cdaa9b968157a
victor kigen <vickigen@gmail.com>
2021-04-12 15:28:01 +0300

    pass title

--- app/views.py
+++ app/views.py
@@ -18,3 +18,12 @@ def movie(movie_id):
     View movie page function that returns the movie details page and its data
     '''
     return render_template('movie.html',id = movie_id)
+
+def index():
+    
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title)
