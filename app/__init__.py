324ed2ef7703460dd38373b469d38e6e05949260
victor kigen <vickigen@gmail.com>
2021-04-13 06:25:31 +0300

    add configurations from instance

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 app = Flask(__name__,instance_relative_config = True)
 
 # Setting up configuration
-app.config.from_object(DevConfig)
+app.config.from_object('DevConfig')
+app.config.from_pyfile('config.py')
 
 from app import views
